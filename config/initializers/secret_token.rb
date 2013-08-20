d0f7aa8e64a58d87273ac6af01a69f648588c18a
candidosales <candidosg@gmail.com>
2013-08-20 19:37:50 -0300

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paydm::Application.config.secret_key_base = 'e0e79f6bf68bbe97ab9990d2715ad9bfa58b89c42470885cf5e157545aa64e5c0ec4864d6265dd59134cb36813dc1efcddac9dfd5d99b9a7a884fd3827631fb4'
