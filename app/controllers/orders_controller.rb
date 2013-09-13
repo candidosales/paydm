ef0adeaa018db0700bb0c5c1dd2798717950286a
candidosales <candidosg@gmail.com>
2013-09-13 00:57:42 -0300

    ok

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,32 +6,32 @@ class OrdersController < ApplicationController
 
   	redirect_to root_path, notice: "Enviamos um e-mail para #{@order.email} confirmando seu pedido." 
 
-    # payment = PagSeguro::PaymentRequest.new
-    # payment.reference = order.id
-    # payment.notification_url = notifications_url
-    # payment.redirect_url = processing_url
+    payment = PagSeguro::PaymentRequest.new
+    payment.reference = order.id
+    payment.notification_url = notifications_url
+    payment.redirect_url = processing_url
 
-    # order.products.each do |product|
-    #   payment.items << {
-    #     id: product.id,
-    #     description: product.title,
-    #     amount: product.price,
-    #     weight: product.weight
-    #   }
-    # end
+    order.products.each do |product|
+      payment.items << {
+        id: product.id,
+        description: product.title,
+        amount: product.price,
+        weight: product.weight
+      }
+    end
 
-    # response = payment.register
+    response = payment.register
 
-    # # Caso o processo de checkout tenha dado errado, lança uma exceção.
-    # # Assim, um serviço de rastreamento de exceções ou até mesmo a gem
-    # # exception_notification poderá notificar sobre o ocorrido.
-    # #
-    # # Se estiver tudo certo, redireciona o comprador para o PagSeguro.
-    # if response.errors.any?
-    #   raise response.errors.join("\n")
-    # else
-    #   redirect_to response.url
-    # end
+    # Caso o processo de checkout tenha dado errado, lança uma exceção.
+    # Assim, um serviço de rastreamento de exceções ou até mesmo a gem
+    # exception_notification poderá notificar sobre o ocorrido.
+    #
+    # Se estiver tudo certo, redireciona o comprador para o PagSeguro.
+    if response.errors.any?
+      raise response.errors.join("\n")
+    else
+      redirect_to response.url
+    end
 
   end
 
