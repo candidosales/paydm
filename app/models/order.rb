1635cb89cd763615d4dc521009586b6fdb27197c
candidosales <candidosg@gmail.com>
2013-12-01 00:40:25 -0300

    tirei os dados pessoais e diminui mais a descricao para capitulo

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,7 @@ class Order < ActiveRecord::Base
 	 		"#{self.tipo.capitalize}, #{self.operation.capitalize}: #{number_to_currency self.price} | Devedor: #{self.name}, CPF: #{self.cpf}, CID: #{self.cid}"
 	 	else
 	 		result = self.qtd_membro*self.price
-	 		"#{self.tipo.capitalize}, #{self.operation.capitalize}: #{number_to_currency self.price} x #{self.qtd_membro} = #{number_to_currency result} | Protocolo: #{self.protocolo}"
+	 		"#{self.operation.capitalize}: #{number_to_currency self.price} x #{self.qtd_membro} = #{number_to_currency result} | Protocolo: #{self.protocolo}"
 	 	end
 	 end
 end
