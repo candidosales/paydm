6c54e1102cf673c3f9ddba6fc07471bc83c29bf8
candidosales <candidosg@gmail.com>
2015-05-21 18:08:22 -0300

    ok

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,5 @@
 ### 2.0.0 (21 de Maio de 2015)
 
 * Atualizado todas as gems para a versão do Rails 4.2;
+* Usando Capistrano 3.1 para deploy;
+* Fazendo migração;
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-    
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
